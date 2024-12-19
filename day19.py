5a72185e305ae3d97053b6278f9603ad47d50e2a
Nejc Zajc <nz44156@student.uni-lj.si>
2024-12-19 06:39:23 +0100

    day 19 ... with memoization!

--- day19.py
+++ day19.py
@@ -5,21 +5,21 @@ from functools import lru_cache
 
 av_by_len = defaultdict(set)
 
+
 @lru_cache()
 def can_make_count(to_make):
 
     if to_make == "":
         return 1
-    
+
     ways_to_make = 0
     for l, towels in av_by_len.items():
         if len(to_make) >= l:
             if to_make[:l] in towels:
                 amount = can_make_count(to_make[l:])
                 ways_to_make += amount
-    
-    return ways_to_make
 
+    return ways_to_make
 
 
 def sol(patterns):
@@ -27,10 +27,9 @@ def sol(patterns):
     total2 = 0
     for p in patterns:
         amount = can_make_count(p)
-        
+
         total1 += 1 if amount else 0
         total2 += amount
-
 
     return total1, total2
 
@@ -44,7 +43,6 @@ if __name__ == "__main__":
     for a in available:
         av_by_len[len(a)].add(a)
     to_make = file[2:]
-    
 
     # both parts
     s1, s2 = sol(to_make)
